486424d93d0f13423055e2302624c01c6ca78c98
Pranathi Peri <pranathiperi@a-pc29w0pr.mynetworksettings.com>
2022-11-16 12:50:04 -0500

    added section headers and hover links

--- components/Svg.js
+++ components/Svg.js
@@ -21,3 +21,22 @@ export const Orb = () => (
     <circle cx="75" cy="75" r="75" fill="url(#orb-gradient)" />
   </svg>
 );
+
+export const Underline = () => (
+  <svg width="100%" height="2" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+      <radialGradient
+        id="orb-gradient"
+        cx="50%"
+        cy="50%"
+        r="50%"
+        fx="50%"
+        fy="50%"
+      >
+        <stop offset="0%" style={{ stopColor: `#FF00FF` }} />
+        <stop offset="100%" style={{ stopColor: `#FFBD2E` }} />
+      </radialGradient>
+    </defs>
+    <rect width="100%" height="2" fill="url(#orb-gradient)" />
+  </svg>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Layout from "../components/Layout";
+import Section from "../components/Section";
 
 const homePage = () => {
   return (
@@ -7,10 +8,13 @@ const homePage = () => {
       <div className="grid max-w-full gap-16 auto-rows-auto">
         <div className="mt-12 md:mt-8">
           <h2>
-            about • music i’ve been listening to • clothes i’ve been making
+            <a href="/">about</a> • <a href="/">music i’ve been listening to</a>{" "}
+            • <a href="/">clothes i’ve been making</a>
           </h2>
         </div>
       </div>
+      <Section title="projects and gigs"></Section>
+      <Section title="let's chat"></Section>
     </Layout>
   );
 };
