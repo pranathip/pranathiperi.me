d37dc6c0470b9c57c23ce98ca604390c3816583f
Pranathi Peri <pranathiperi@pranathis-mbp.lan>
2022-11-23 11:31:17 -0600

    refactoring incoming transition:

--- components/Layout.js
+++ components/Layout.js
@@ -1,54 +1,75 @@
 import Header from "./Header";
 import React from "react";
 import Head from "next/head";
+import { Transition } from "@headlessui/react";
+import { useEffect, useState } from "react";
 
 const Layout = ({ children }) => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShow(true);
+    }, 600);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
-    <main>
-      <Head>
-        <title>Pranathi Peri</title>
-        <link
-          href="https://pranathiperi.me/images/icons/favicon-96x96.png"
-          rel="icon"
-          sizes="96x96"
-          type="image/png"
-        />
-        <link
-          href="https://pranathiperi.me/images/icons/favicon-32x32.png"
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          href="https://pranathiperi.me/images/icons/favicon-16x16.png"
-          rel="icon"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link
-          href="https://pranathiperi.me/images/icons/apple-icon-60x60.png"
-          rel="apple-touch-icon"
-          sizes="60x60"
-        />
-        <link
-          href="https://pranathiperi.me/images/icons/apple-icon-72x72.png"
-          rel="apple-touch-icon"
-          sizes="72x72"
-        />
-        <link
-          href="https://pranathiperi.me/images/icons/apple-icon-120x120.png"
-          rel="apple-touch-icon"
-          sizes="120x120"
-        />
-      </Head>
-      <div className="container mx-auto mt-16 max-w-container-small md:max-w-container-large">
-        <Header />
-        {children}
-        <div className="flex flex-row justify-between my-8">
-          <p>made with ♡ in nyc by pranathi peri</p>
+    <Transition
+      appear={true}
+      show={show}
+      as="div"
+      enter="transition duration-500 ease-in-out"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition duration-500 ease-in-out"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <main>
+        <Head>
+          <title>Pranathi Peri</title>
+          <link
+            href="https://pranathiperi.me/images/icons/favicon-96x96.png"
+            rel="icon"
+            sizes="96x96"
+            type="image/png"
+          />
+          <link
+            href="https://pranathiperi.me/images/icons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="https://pranathiperi.me/images/icons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+          <link
+            href="https://pranathiperi.me/images/icons/apple-icon-60x60.png"
+            rel="apple-touch-icon"
+            sizes="60x60"
+          />
+          <link
+            href="https://pranathiperi.me/images/icons/apple-icon-72x72.png"
+            rel="apple-touch-icon"
+            sizes="72x72"
+          />
+          <link
+            href="https://pranathiperi.me/images/icons/apple-icon-120x120.png"
+            rel="apple-touch-icon"
+            sizes="120x120"
+          />
+        </Head>
+        <div className="container mx-auto mt-16 max-w-container-small md:max-w-container-large">
+          <Header />
+          {children}
+          <div className="flex flex-row justify-between my-8">
+            <p>made with ♡ in nyc by pranathi peri</p>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </Transition>
   );
 };
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -3,81 +3,60 @@ import Layout from "../components/Layout";
 import Section from "../components/Section";
 import ContactInfo from "../components/ContactInfo";
 import ProjectSection from "../components/ProjectSection";
-import { Transition } from "@headlessui/react";
-import { useEffect, useState } from "react";
 
 const homePage = () => {
-  const [show, setShow] = useState(false);
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setShow(true);
-    }, 600);
-    return () => clearTimeout(timeout);
-  }, []);
   return (
-    <Transition
-      appear={true}
-      show={show}
-      as="div"
-      enter="transition duration-500 ease-in-out"
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      leave="transition duration-500 ease-in-out"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-    >
-      <Layout>
-        <div className="grid max-w-full auto-rows-auto">
-          <div className="mt-8">
-            <h2>
-              <a href="/about">about</a> •{" "}
-              <a href="https://open.spotify.com/user/pperi13?si=6d771ffe169f47ff">
-                music i’ve been listening to
-              </a>{" "}
-              •{" "}
-              <a href="https://instagram.com/durgathelabel">
-                clothes i’ve been making
-              </a>
-            </h2>
-          </div>
+    <Layout>
+      <div className="grid max-w-full auto-rows-auto">
+        <div className="mt-8">
+          <h2>
+            <a href="/about">about</a> •{" "}
+            <a href="https://open.spotify.com/user/pperi13?si=6d771ffe169f47ff">
+              music i’ve been listening to
+            </a>{" "}
+            •{" "}
+            <a href="https://instagram.com/durgathelabel">
+              clothes i’ve been making
+            </a>
+          </h2>
         </div>
-        <Section title="projects and gigs" showLastUpdated>
-          <ProjectSection
-            subject="product designer"
-            project="github"
-            date="mar 2022 - sep 2022"
-            subtitle="made pull requests even better. designed flows for merge queue, threaded comments on PRs, and some other [secret stuff].  "
-          ></ProjectSection>
-          <ProjectSection
-            subject="software engineering intern"
-            project="facebook"
-            date="may 2020 - aug 2020"
-            subtitle="designed and developed features for messenger kids on iOS"
-          ></ProjectSection>
-          <ProjectSection
-            subject="product design lead"
-            project="berkeley mobile"
-            date="aug 2020 - dec 2021"
-            subtitle="led the design and feature development for uc berkeley's flagship mobile application, available on the app store and used daily by thousands of students."
-          ></ProjectSection>
-          <ProjectSection
-            subject="deadstock"
-            project="personal project (iOS application)"
-            date="aug 2020"
-            subtitle="deadstock is an app that allows users to monitor volatile prices of highly sought after sneakers. design and developed from the ground up"
-          ></ProjectSection>
-          <ProjectSection
-            subject="founder and designer"
-            project="durga the label"
-            date="jan 2022 - present"
-            subtitle="hand making modern clothing pieces out of sustainably sourced indian textiles and fabrics."
-          ></ProjectSection>
-        </Section>
-        <Section title="let's chat">
-          <ContactInfo />
-        </Section>
-      </Layout>
-    </Transition>
+      </div>
+      <Section title="projects and gigs" showLastUpdated>
+        <ProjectSection
+          subject="product designer"
+          project="github"
+          date="mar 2022 - sep 2022"
+          subtitle="made pull requests even better. designed flows for merge queue, threaded comments on PRs, and some other [secret stuff].  "
+        ></ProjectSection>
+        <ProjectSection
+          subject="software engineering intern"
+          project="facebook"
+          date="may 2020 - aug 2020"
+          subtitle="designed and developed features for messenger kids on iOS"
+        ></ProjectSection>
+        <ProjectSection
+          subject="product design lead"
+          project="berkeley mobile"
+          date="aug 2020 - dec 2021"
+          subtitle="led the design and feature development for uc berkeley's flagship mobile application, available on the app store and used daily by thousands of students."
+        ></ProjectSection>
+        <ProjectSection
+          subject="deadstock"
+          project="personal project (iOS application)"
+          date="aug 2020"
+          subtitle="deadstock is an app that allows users to monitor volatile prices of highly sought after sneakers. design and developed from the ground up"
+        ></ProjectSection>
+        <ProjectSection
+          subject="founder and designer"
+          project="durga the label"
+          date="jan 2022 - present"
+          subtitle="hand making modern clothing pieces out of sustainably sourced indian textiles and fabrics."
+        ></ProjectSection>
+      </Section>
+      <Section title="let's chat">
+        <ContactInfo />
+      </Section>
+    </Layout>
   );
 };
 export default homePage;
