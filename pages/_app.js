dd2daaadb4a1e5922d0442b4dc316a1ca93ee120
Pranathi Peri <pranathiperi@pranathis-mbp.lan>
2022-11-24 23:48:25 -0600

    so thats why analytics wasn't working

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,10 @@ import { Analytics } from "@vercel/analytics/react";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-  <Analytics />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />;
+    </>
+  );
 }
